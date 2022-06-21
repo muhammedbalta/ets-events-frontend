40427c1017e6fb0c9401251401a94c5f07673d8c
Muhammed Balta <muhammedbalta07@gmail.com>
2022-06-21 23:04:42 +0300

    Showcase module

---
+++ components/Showcase.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "@/styles/Showcase.module.css";
+function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>ETS Events Showcase</h1>
+      <h2>Find Events Near Your Location</h2>
+    </div>
+  );
+}
+
+export default Showcase;
